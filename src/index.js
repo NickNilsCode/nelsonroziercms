c250186cca0e9b488b12886bd8787253bbc5b8cc
seoagencyanalytics <109499326+seoagencyanalytics@users.noreply.github.com>
2022-07-28 10:46:34 +0530

    Revert "Feat/google gtm"

--- src/index.js
+++ src/index.js
@@ -166,47 +166,17 @@ function returnHTML(data, bundle, Page, title){
                 <script src="https://kit.fontawesome.com/7fa747235e.js" crossorigin="anonymous"></script>
 
                 ${styles}
-		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180738835-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-180738835-1');
-</script>
-		
-		<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KGNFJWW');</script>
-<!-- End Google Tag Manager -->
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JHY6MME7XL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JHY6MME7XL');
-</script>
 
               </head>
               <body>
                 <script>window.os = window.os || {};</script>
                 <script>window.__DATA__=${dataString}</script>
                 <div id="app" role="main">${body}</div>
-                 
-		 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGNFJWW"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
-		 
+                  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-KGNFJWW');</script>
                 <script>${bundle}</script>
               </body>
             </html>
