faf6bd24bad799ff0c728ee3c70c6d407c9514e6
seoagencyanalytics <109499326+seoagencyanalytics@users.noreply.github.com>
2022-08-04 05:58:13 +0300

    Add GTM Codes

--- src/index.js
+++ src/index.js
@@ -167,6 +167,36 @@ function returnHTML(data, bundle, Page, title){
 
                 ${styles}
 
+                <meta name="google-site-verification" content="QcxUyvtxY2put4kvP8hB4qGQ364ORLZJjLOujBHmgx4" />
+
+                <!-- Global site tag (gtag.js) - Google Analytics -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180738835-1"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'UA-180738835-1');
+                </script>
+
+                <!-- Google tag (gtag.js) -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-JHY6MME7XL"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'G-JHY6MME7XL');
+                </script>
+
+                <!-- Google Tag Manager -->
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KGNFJWW');</script>
+                <!-- End Google Tag Manager -->                
+
               </head>
               <body>
                 <script>window.os = window.os || {};</script>
@@ -178,6 +208,12 @@ function returnHTML(data, bundle, Page, title){
                   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                   })(window,document,'script','dataLayer','GTM-KGNFJWW');</script>
                 <script>${bundle}</script>
+
+                <!-- Google Tag Manager (noscript) -->
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGNFJWW"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                <!-- End Google Tag Manager (noscript) -->
+
               </body>
             </html>
           `;
